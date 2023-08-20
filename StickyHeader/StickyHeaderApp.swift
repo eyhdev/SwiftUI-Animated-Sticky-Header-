b4ad733b4a263c1e008bb049dc71d6ca644d86af
eyhdev <alaqsa335@gmail.com>
2023-08-20 12:29:51 +0300

    Initial Commit

---
+++ StickyHeader/StickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyHeaderApp.swift
+//  StickyHeader
+//
+//  Created by eyh.mac on 20.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct StickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
