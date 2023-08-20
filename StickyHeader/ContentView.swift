38c1c5a8c14dd550bc1c153ed95f6921b55cb37a
eyhdev <alaqsa335@gmail.com>
2023-08-20 14:08:06 +0300

    SwiftUI | Animated Sticky Header

--- StickyHeader/ContentView.swift
+++ StickyHeader/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader{
+            let safeArea = $0.safeAreaInsets
+            let size = $0.size
+            
+            Home(safeArea: safeArea, size: size)
+                .ignoresSafeArea(.container, edges: .top)
         }
-        .padding()
+        .preferredColorScheme(.dark)
     }
 }
 
